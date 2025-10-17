b103f1155916a6733c7f26b225a0acc5dec30fa3
Jan Burgy <jburgy@gmail.com>
2025-10-17 11:07:06 -0400

    Initial revision

---
+++ index.js
@@ -0,0 +1,21 @@
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event.request));
+});
+
+
+/**
+ * Add Cross-Origin-Resource-Policy headers.
+ * @param {Request} request
+ * @returns {Promise<Response>}
+ */
+async function handleRequest(request) {
+    const { pathname } = new URL(request.url);
+    const response = await fetch(new URL(pathname, 'https://giscus.app'));
+    const headers = new Headers(response.headers);
+    headers.set('Cross-Origin-Resource-Policy', 'cross-origin');
+    return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: headers
+    });
+}
